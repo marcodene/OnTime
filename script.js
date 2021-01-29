b397bee9adfb32db119b60d608f9aca9328df0c1
marcodene <65088375+marcodene@users.noreply.github.com>
2021-01-29 17:23:44 +0100

    add a counter and an alert in case of winning or loosing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var image = document.querySelector("#image");
+const imageElem = document.querySelector("#image");
+const countElem = document.querySelector("#count");
 
 //immagine formata da 24 tick, quindi, affinche la rivoluzione avvenga in 2 secondi, ogni 8ms si deve muovere di 15 gradi
 const tick = 24;
@@ -9,19 +10,38 @@ const velocity = (completeRotation * degreeToAdd) / 360;
 //360:2000=15:x
 
 var degree = 0;
+var count = 0;
 var loop;
 
 const handleClick = () => {
   if (loop) {
     clearInterval(loop);
+    checkForWin();
+    degree = 0;
     loop = false;
   } else {
     loop = setInterval(rotate, velocity);
   }
 };
 
-function rotate() {
+const rotate = () => {
   console.log(degree);
-  image.style.transform = `rotate(${degree}deg)`;
   degree += 15;
-}
+  imageElem.style.transform = `rotate(${degree}deg)`;
+};
+
+const checkForWin = () => {
+  console.log("degree:", degree);
+  if (degree % 360 === 0) {
+    //the yellow tick is on the top
+    alert("you won");
+    incrementCount();
+  } else {
+    alert("you lost");
+  }
+};
+
+const incrementCount = () => {
+  count += 1;
+  countElem.innerHTML = count;
+};
